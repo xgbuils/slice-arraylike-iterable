074e06c7fac5315c5789e6da20e0aec602501c42
Xavier Garcia Buils <xgbuils@gmail.com>
2017-08-07 21:37:17 +0200

    add fn-reduce dependency

--- index.js
+++ index.js
@@ -1,15 +1,8 @@
 const sliceCalculator = require('slice-calculator')
-
-function arrayReduce (f, acc, arr) {
-    const length = arr.length
-    for (let i = 0; i < length; ++i) {
-        acc = f(acc, arr[i])
-    }
-    return acc
-}
+const reduce = require('fn-reduce')
 
 function descriptorsFactory (props) {
-    return arrayReduce((descriptors, prop) => {
+    return reduce((descriptors, prop) => {
         descriptors[prop] = {
             value: props[prop]
         }
