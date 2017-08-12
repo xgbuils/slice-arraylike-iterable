a7eab589da83af22fcef1495a42f64979c7d2795
Xavier Garcia Buils <xgbuils@gmail.com>
2017-08-12 21:29:39 +0200

    defineProperties approach

--- index.js
+++ index.js
@@ -1,33 +1,35 @@
-class AbstractSliceIterable {
-    slice (start, end) {
-        start = Math.max(start, 0)
-        const newEnd = Math.min(this.start + end, this.end)
-        if (start === 0 && newEnd === this.end) {
-            return this
-        }
-        const obj = Object.create(SliceArrayLikeIterable.prototype)
-        obj.start = this.start + start
-        obj.end = newEnd
-        obj.iterable = this.iterable
-        return obj
+function slice (start, end) {
+    start = Math.max(start, 0)
+    const newEnd = Math.min(this.start + end, this.end)
+    if (start === 0 && newEnd === this.end) {
+        return this
     }
+    const obj = Object.create(SliceArrayLikeIterable.prototype)
+    obj.start = this.start + start
+    obj.end = newEnd
+    obj.iterable = this.iterable
+    return obj
 }
 
-class SliceArrayLikeIterable extends AbstractSliceIterable {
-    constructor (iterable) {
-        super()
-        this.iterable = iterable
-        this.start = 0
-        this.end = iterable.length
-    }
+function SliceArrayLikeIterable (iterable) {
+    this.iterable = iterable
+    this.start = 0
+    this.end = iterable.length
+}
 
-    * [Symbol.iterator] () {
-        const iterable = this.iterable
-        const end = this.end
-        for (let i = this.start; i < end; ++i) {
-            yield iterable[i]
+Object.defineProperties(SliceArrayLikeIterable.prototype, {
+    slice: {
+        value: slice
+    },
+    [Symbol.iterator]: {
+        * value () {
+            const iterable = this.iterable
+            const end = this.end
+            for (let i = this.start; i < end; ++i) {
+                yield iterable[i]
+            }
         }
     }
-}
+})
 
 module.exports = SliceArrayLikeIterable
